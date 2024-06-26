fc0160c67d5d5f12976a24fcc8ac523e5fea7184
Romeo509 <romeoameku503@gmail.com>
2024-06-26 16:32:09 +0000

    Objective: Run checker again

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,45 +1,52 @@
 #!/usr/bin/env python3
 """
-MRUCache module
+MRU Caching System Module
 """
 
 from base_caching import BaseCaching
+from collections import OrderedDict
 
 
 class MRUCache(BaseCaching):
-    """ MRUCache class
-    Inherits from BaseCaching and implements an MRU caching system
+    """
+    MRUCache class that inherits from BaseCaching and
+    implements a MRU caching system
     """
 
     def __init__(self):
-        """ Initialize
-        """
+        """ Initialize the MRUCache """
         super().__init__()
-        self.order = []
+        self.order = OrderedDict()
 
     def put(self, key, item):
-        """ Add an item in the cache
+        """
+        Add an item in the cache
+        If the number of items exceeds MAX_ITEMS,
+        discard the most recently used item
         """
         if key is None or item is None:
             return
 
         if key in self.cache_data:
-            self.order.remove(key)
+            self.cache_data[key] = item
+            self.order.move_to_end(key)
+        else:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                discard_key = next(reversed(self.order))
+                print(f"DISCARD: {discard_key}")
+                del self.cache_data[discard_key]
+                del self.order[discard_key]
 
-        self.cache_data[key] = item
-        self.order.append(key)
+            self.cache_data[key] = item
+            self.order[key] = None
 
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            mru_key = self.order.pop()
-            del self.cache_data[mru_key]
-            print("DISCARD:", mru_key)
+        self.order.move_to_end(key)
 
     def get(self, key):
-        """ Get an item by key
+        """
+        Get an item by key
         """
         if key is None or key not in self.cache_data:
             return None
-
-        self.order.remove(key)
-        self.order.append(key)
+        self.order.move_to_end(key)
         return self.cache_data[key]
