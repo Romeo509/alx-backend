04705bcca394dea1b852a7e98f5e7db1e3f485b3
Romeo509 <romeoameku503@gmail.com>
2024-06-26 02:46:42 +0000

    test the function

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+BasicCache module
+"""
+
+from base_caching import BaseCaching
+
+class BasicCache(BaseCaching):
+    """ BasicCache class
+    Inherits from BaseCaching and is a basic caching system
+    """
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        return self.cache_data.get(key, None)
